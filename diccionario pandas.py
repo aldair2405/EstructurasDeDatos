ebfa7feb0e9698438591513d127e579901b2eb11
Emerson Rodriguez <emersonrdz04@gmail.com>
2024-10-19 07:45:25 -0600

    Add files via upload

---
+++ diccionario pandas.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+
+diccionario_python={
+    "A":10,
+    "B":20,
+    "C":30,
+    "D":40,
+    "E":50,
+}
+edades_diccionario=pd.Series(diccionario_python)
+edades_diccionario
